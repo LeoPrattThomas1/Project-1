4ddd88271c990ee9a55f47f32aced25cd7c93750
LeoPrattThomas1 <leogpt@icloud.com>
2025-01-13 20:36:39 +0000

    Added main comments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
-#include "mbed.h"
+//=====[Libraries]=============================================================
 
-// main() runs in its own thread in the OS
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+//=====[Defines]===============================================================
+
+//Null
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+
+//=====[Main function, the program entry point after power on or reset]========
+
 int main()
 {
+    inputsInit();
+    outputsInit();
     while (true) {
-
+        alarmActivationUpdate();
+        alarmDeactivationUpdate();
+        uartTask();
+        delay(TIME_INCREMENT_MS);
     }
 }
+
 
