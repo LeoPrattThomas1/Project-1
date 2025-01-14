2df6c56d7e12b280a49d5468e7a6f434480b2b19
LeoPrattThomas1 <leogpt@icloud.com>
2025-01-14 16:51:58 +0000

    cleaned up

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 #include "mbed.h"
 #include "arm_book_lib.h"
-#include <string>
 
 //=====[Defines]===============================================================
 
@@ -48,11 +47,7 @@ int main()
 
     part1();
     part2();
-
     part3();
-    /*while (true){ 
-        //alarm_buzzer.output();  
-    }*/
 }
 
 void inputsInit()
@@ -81,12 +76,10 @@ Light the green LED only when both seats are occupied and both seatbelts are fas
 
 void part1(){
     //When the driver sits down, //display the message, “Welcome to enhanced alarm system model 218-W24”.
-    
 
     //while the button is not presseed wait till the button is pressed.
-    while (driver_occupancy == OFF){
-        //None
-    }
+    while (driver_occupancy == OFF){ }
+
     uartUsb.write( "Welcome to enhanced alarm system model 218-W24\r\n", 48 );
 
 }
@@ -132,7 +125,6 @@ void part3(){
 
 
     } else {
-//      then ignition is inhibited. 
 
 
 //      Sound the alarm buzzer; 
@@ -145,134 +137,15 @@ void part3(){
 
         if (!driver_occupancy){// driver_occupancy
             uartUsb.write( "Driver seat not occupied\r\n", 19+6+2);
-        } if (!passenger_occupancy){// paxwssenger_occupancy
+
+        } if (!passenger_occupancy){// pasenger_occupancy
             uartUsb.write( "Passenger seat not occupied\r\n", 19+9+2);
+
         } if (!driver_seatbelt){// driver_seatbelt
             uartUsb.write( "Driver seatbelt not fastened\r\n", 22+6+2);
+
         } if (!passenger_seatbelt){// passenger_seatbelt
             uartUsb.write( "Passenger seatbelt not fastened\r\n", 22+9+2);
-//              “Passenger seat not occupied,” “Driver seatbelt not fastened,” etc. 
         }
     }
-
 }
-
-
-
-
-
-
-
-//Light the green LED 
-//  only when 
-//      (both seats are occupied) AND (both seatbelts are fastened)
-
-
-
-/*
-void alarmActivationUpdate()
-{
-    static int lm35SampleIndex = 0;
-    int i = 0;
-
-    lm35ReadingsArray[lm35SampleIndex] = lm35.read();
-    lm35SampleIndex++;
-    if ( lm35SampleIndex >= NUMBER_OF_AVG_SAMPLES) {
-        lm35SampleIndex = 0;
-    }
-    
-       lm35ReadingsSum = 0.0;
-    for (i = 0; i < NUMBER_OF_AVG_SAMPLES; i++) {
-        lm35ReadingsSum = lm35ReadingsSum + lm35ReadingsArray[i];
-    }
-    lm35ReadingsAverage = lm35ReadingsSum / NUMBER_OF_AVG_SAMPLES;
-       lm35TempC = analogReadingScaledWithTheLM35Formula ( lm35ReadingsAverage );    
-    
-    if ( lm35TempC > OVER_TEMP_LEVEL ) {
-        overTempDetector = ON;
-    } else {
-        overTempDetector = OFF;
-    }
-
-    if( !mq2) {
-        gasDetectorState = ON;
-        alarmState = ON;
-    }
-    if( overTempDetector ) {
-        overTempDetectorState = ON;
-        alarmState = ON;
-    }
-    if( alarmTestButton ) {             
-        overTempDetectorState = ON;
-        gasDetectorState = ON;
-        alarmState = ON;
-    }    
-    if( alarmState ) { 
-        accumulatedTimeAlarm = accumulatedTimeAlarm + TIME_INCREMENT_MS;
-        sirenPin.output();                                     
-        sirenPin = LOW;                                        
-    
-        if( gasDetectorState && overTempDetectorState ) {
-            if( accumulatedTimeAlarm >= BLINKING_TIME_GAS_AND_OVER_TEMP_ALARM ) {
-                accumulatedTimeAlarm = 0;
-                alarmLed = !alarmLed;
-            }
-        } else if( gasDetectorState ) {
-            if( accumulatedTimeAlarm >= BLINKING_TIME_GAS_ALARM ) {
-                accumulatedTimeAlarm = 0;
-                alarmLed = !alarmLed;
-            }
-        } else if ( overTempDetectorState ) {
-            if( accumulatedTimeAlarm >= BLINKING_TIME_OVER_TEMP_ALARM  ) {
-                accumulatedTimeAlarm = 0;
-                alarmLed = !alarmLed;
-            }
-        }
-    } else{
-        alarmLed = OFF;
-        gasDetectorState = OFF;
-        overTempDetectorState = OFF;
-        sirenPin.input();                                  
-    }
-}
-*/
-
-/*
-Typical Driver’s Ed. vehicles are already equipped with the following devices: 
-Driver seat occupancy sensor (provides contact closure when occupied);
-
-BTN
-
-
-Passenger seat occupancy sensor (provides contact closure when occupied);
-
-BTN
-
-
-Driver seatbelt sensor (provides contact closure when fastened);
-
-SW
-
-
-Passenger seatbelt sensor (provides contact closure when fastened);
-
-SW
-
-
-Ignition push button (provides contact closure to request ignition);
-
-BTN
-
-
-
-Alarm buzzer (sounds when ignition is inhibited);
-
-buzzer
-
-
-Green indicator LED (lights when ignition is enabled);
- Blue indicator LED (lights when the engine has started);
-Display monitor (displays welcome and error messages);
-*/
-
-
